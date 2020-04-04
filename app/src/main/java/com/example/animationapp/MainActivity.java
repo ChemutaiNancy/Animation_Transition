54abef71e3d471eebe2a7135f3b80b3f0f498d2f
Nancy <chemutain@gmail.com>
2020-04-05 00:06:23 +0300

    Animation and transition

---
+++ app/src/main/java/com/example/animationapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.animationapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewGroup myLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLayout = findViewById(R.id.myLayout);
+
+        myLayout.setOnTouchListener(new RelativeLayout.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                moveButton();
+                return true;
+            }
+        });
+    }
+
+    private void moveButton() {
+
+        TransitionManager.beginDelayedTransition(myLayout);
+        View myButton = findViewById(R.id.my_button);
+
+//        give rules to the layout
+        RelativeLayout.LayoutParams positionRules = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        positionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        myButton.setLayoutParams(positionRules);
+
+//        code for expanding the button
+        ViewGroup.LayoutParams sizeRules = myButton.getLayoutParams();
+        sizeRules.width=450;
+        sizeRules.height=300;
+        myButton.setLayoutParams(sizeRules);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
